6795a77c34ebdd6f818a8035af4f456f2e6f76a4
Rasmi Roy <rasmiroy@yahoo.com>
2024-01-30 21:27:00 -0500

    added new javascript file and updated html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,65 @@
 //https://api.openweathermap.org/data/2.5/weather?q=Miami&appid=5b83fe4bc0e5a5e00702d6628342934c
 
-var formEl = document.getElementById("form")
-var textboxEl = document.getElementById("cname")
 var weather_APIKey = "5b83fe4bc0e5a5e00702d6628342934c"
-var dayJS = dayjs()
-var currentDate = dayJS.format("M/D/YYYY")
-var cityName
-//var weatherData = []
+var currentDashboardEl = document.getElementById("current-ds")
+var forecastDashboardEl = document.getElementById("forecast-ds")
+var searchBtnEl = document.getElementById("search-btn")
+var currentContainerEl= document.getElementById("current-ds-c")
+var forecastTitleEl = document.getElementById("forecast-title")
 
 
-/*fetch(queryURL)
-.then(function(response) {
-    return response.json()
-})
-.then(function(data) {
-    console.log(data)
-   // return data
-})*/
+function currentWeather() {
+    var cityName = document.getElementById("city-name").value
+    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${weather_APIKey}&units=imperial`
+    currentContainerEl.classList.remove("hide")
 
-function retrieveWeatherData(event) {
-    event.preventDefault()
-    cityName = textboxEl.value
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+weather_APIKey
-    //console.log(queryURL)
     fetch(queryURL)
     .then(function(response) {
         return response.json()
     })
     .then(function(data) {
-        /*console.log(data.main.temp)
-        console.log(data.name)
-        console.log(data.wind.speed)
-        console.log(data.main.humidity)*/
-        var cityWeatherData = {
-            cityname : data.name,
-            temperature : data.main.temp,
-            wind : data.wind.speed,
-            humidity : data.main.humidity,
-            icon : data.weather[0].icon
+        console.log(data)
+        var unixTimestamp = data.dt
+        var date = new Date(unixTimestamp * 1000)
+        var convertTimestamp = date.toLocaleDateString("en-US")
+        currentDashboardEl.innerHTML = `<h4>${data.name} ${convertTimestamp}<img src='http://openweathermap.org/img/w/${data.weather[0].icon}.png'></h3>
+        <p class="m-1">Temp: ${data.main.temp}&degF</p>
+        <p class="m-1">Wind: ${data.wind.speed}MPH</p>
+        <p class="m-1">Humidity: ${data.main.humidity}%</p>`
+    })
+    
+    fivedayForecast(cityName)
+    searchHistory(cityName)
+}
+
+function fivedayForecast(cityName) {
+    var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${weather_APIKey}&units=imperial`
+    forecastDashboardEl.innerHTML = ""
+    currentDashboardEl.classList.remove("hide")
+    forecastTitleEl.classList.remove("hide")
+    forecastDashboardEl.classList.remove("hide")
+
+    fetch(queryURL)
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data) {
+        //console.log(data)
+
+        for(var i=3; i < data.list.length; i+=8) {
+            
+            var unixTimestamp = data.list[i].dt
+            var date = new Date(unixTimestamp * 1000)
+            var convertTimestamp = date.toLocaleDateString("en-US")
+            console.log(convertTimestamp)
+
+            forecastDashboardEl.innerHTML += `<div class="col-sm-2"><div class="card custom-card-bg"><div class="card-body"><h5 class="card-title">${convertTimestamp}<img src="http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png"></h5><p class="card-text m-1">Temp: ${data.list[i].main.temp}&degF</p><p class="card-text m-1">Wind: ${data.list[i].wind.speed}MPH</p><p class="card-text m-1">Humidity: ${data.list[i].main.humidity}%</p></div></div></div>`
         }
-        displayWeatherData(data)
-//console.log(data)
-//console.log(cityWeatherData)
-    // return data
     })
 }
 
-formEl.addEventListener("submit", retrieveWeatherData)
+//currentWeather('Miami')
+//fivedayForecast('Miami')
+
+
+searchBtnEl.addEventListener("click", currentWeather)
